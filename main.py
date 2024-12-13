0fce1fc04e7003540ba4e477a41d33b60d50f64a
DESTINY WANJIRU <dwngotho@gmail.com>
2024-12-13 13:00:57 +0300

    tuple sets guys!!!

---
+++ main.py
@@ -0,0 +1,13 @@
+t=() #empty tuple
+t=(4,8,2)
+print(t)
+nt=('mouse',[0,3,5] ,(12,45,78))
+print(nt[0] [3])
+print(nt[2] [1])
+ot=(98,87,23,24)
+print(ot[3])
+newtuple=('destiny')
+for i in newtuple:
+    print(i)
+
+
