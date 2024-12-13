fbd2744386d24a755ba240c0d4da1f7b5ae25b7f
DESTINY WANJIRU <dwngotho@gmail.com>
2024-12-13 13:09:43 +0300

    sets

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-t=() #empty tuple
-t=(4,8,2)
-print(t)
-nt=('mouse',[0,3,5] ,(12,45,78))
-print(nt[0] [3])
-print(nt[2] [1])
-ot=(98,87,23,24)
-print(ot[3])
-newtuple=('destiny')
-for i in newtuple:
-    print(i)
+# t=() #empty tuple
+# t=(4,8,2)
+# print(t)
+# nt=('mouse',[0,3,5] ,(12,45,78))
+# print(nt[0] [3])
+# print(nt[2] [1])
+# ot=(98,87,23,24)
+# print(ot[3])
+# newtuple=('destiny')
+# for i in newtuple:
+#     print(i)
+s={6,7,8,8,9,9,9,8,8,2}
+print(s)
+a={1,2,3,4,5}
+b={2,4,9,0,8}
+print(a.intersection(b))
+print(a.union(b))
+print(a.difference(b))
+print(b.difference(a))
+print(a.symmetric_difference(b))
 
 
